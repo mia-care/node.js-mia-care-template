9ccb7d2084ff56f103157779fffc535eed4626f4
Davide Calabrò <davide.calabro@mia-platform.eu>
2023-08-21 16:36:38 +0200

    [MCRDMPE-207] Fix branch name checking by adding canonical branch name to script checker

--- scripts/check_branch_naming.sh
+++ scripts/check_branch_naming.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 local_branch_name="$(git rev-parse --abbrev-ref HEAD)"
 
-valid_branch_regex='^(feat|fix|doc)-[A-Z]+-[0-9]+(.*)$'
+valid_branch_regex='^(((feat|fix|doc)-[A-Z]+-[0-9]+(.*))|master|main|(release\/v[0-9]+.[0-9]+.[0-9]+))$'
 
 message="There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
 

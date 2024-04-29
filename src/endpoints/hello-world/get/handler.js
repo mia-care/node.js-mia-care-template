506ee3925722d013837682dc7a3694fcb3f582db
Nicola Moretto <nicola.moretto@mia-care.io>
2024-04-29 17:49:29 +0200

    Add audit log example to the hello world endpoint

--- src/endpoints/hello-world/get/handler.js
+++ src/endpoints/hello-world/get/handler.js
@@ -1,6 +1,7 @@
 'use strict'
 
 async function handler(request, reply) {
+  request.log.audit({ event: 'MiaCare/HelloWorld/v1' }, 'Hello World Audit Log')
   reply.code(200).send({ status: 200, message: 'Hello World' })
 }
 

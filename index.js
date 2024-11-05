d029d1e5ea60b9ec2174043f349a5adb4f8222b3
Nicola Moretto <nicola.moretto@mia-care.io>
2024-11-05 12:14:58 +0100

    Update documentation and implementation for sidecar architecture [MCRDAL-70]

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const customService = require('@mia-platform/custom-plugin-lib')(envVarsSchema)
 
 const getHelloWorld = require('./src/endpoints/hello-world/get')
 
-const { logMethod } = require('./src/lib/utils')
 const { AUDIT_TRAIL_LOG_LEVEL } = require('./src/lib/constants')
 
 module.exports = customService(async function index(service) {
@@ -38,9 +37,6 @@ module.exports.options = {
   logger: {
     customLevels: {
       audit: AUDIT_TRAIL_LOG_LEVEL,
-    },
-    hooks: {
-      logMethod,
     },
   },
 }
